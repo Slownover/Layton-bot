5a48ed9a5f9b1b28bcfb6b122ed2684603dba73b
Slownover <slownover@cybertemp.xyz>
2025-05-14 10:54:41 +0400

    Ep 8

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,7 +1,10 @@
 module.exports = {
   name: 'ready',
   once: true,
-  execute(client) {
+  async execute(client) {
     console.log('Je suis prêt!');
+
+    const devGuild = client.guilds.cache.get('YOUR SERVER ID HERE');
+    devGuild.commands.set(client.commands.map(cmd => cmd));
   },
 };
